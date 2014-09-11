ada2a3c9555de340a29b16f72cb0b2217fcd764e
Cat Cyborg <thecatcyborg@gmail.com>
2014-09-11 18:10:10 +0800

    Add initial iOS and middleman project templates

---
+++ middleman/crud_api_consumer_app/config.rb
@@ -0,0 +1,72 @@
+page "/partials/*.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+after_configuration do
+  sprockets.append_path File.join root.to_s, "bower_components"
+end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # activate :minify_html
+
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  activate :gzip
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # deploy.remote = "production" # remote name or git url, default: origin
+  deploy.branch   = "master"
+  deploy.build_before = true
+end
+
+activate :i18n
+activate :directory_indexes
